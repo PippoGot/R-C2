1094595e2dc8b65fe90879f56b15ebf3be26b35d
Edoardo Di Pietrantonio <citeracitera@gmail.com>
2023-12-19 13:53:35 +0100

    reordered regulation model

--- init.m
+++ init.m
@@ -1,5 +1,6 @@
-initial_conditions = [1, 1, pi/2];
+initial_conditions = [10, 7, pi/2];
+initial_conditions_2 = [-8, -9, 0];
 
 initial_conditions_trans = state_transformation([0,0,0], initial_conditions);
 
-initial_conditions_trans_2 = state_transformation([0,0,0], [-1, 0, 0]);
+initial_conditions_trans_2 = state_transformation([0,0,0], initial_conditions_2);
