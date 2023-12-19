63db8d6a648199d41b3588e2da754282e0c96913
Edoardo Di Pietrantonio <citeracitera@gmail.com>
2023-12-19 16:25:52 +0100

    tracking + consensus

--- init.m
+++ init.m
@@ -1,6 +1,9 @@
-initial_conditions = [10, 7, pi/2];
-initial_conditions_2 = [-8, -9, 0];
+initial_conditions = [10, 10, pi/2];
+initial_conditions_2 = [-10, -9, 0];
+initial_conditions_3 = [10, -9, 0];
 
 initial_conditions_trans = state_transformation([0,0,0], initial_conditions);
 
 initial_conditions_trans_2 = state_transformation([0,0,0], initial_conditions_2);
+
+initial_conditions_trans_3 = state_transformation([0,0,0], initial_conditions_3);
