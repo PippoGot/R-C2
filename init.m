3a3598705f18918a1777880c78dcadff6a2f0980
Edoardo Di Pietrantonio <citeracitera@gmail.com>
2023-12-21 18:58:04 +0100

    all  controllers implemented

--- init.m
+++ init.m
@@ -1,8 +1,8 @@
-initial_conditions = [10, 10, pi/2];
-initial_conditions_2 = [-10, -9, 0];
-initial_conditions_3 = [10, -9, 0];
+initial_conditions_1 = [8, 8, pi*1.8];
+initial_conditions_2 = [8, 8, pi/2];
+initial_conditions_3 = [-8, -8, 3*pi/2];
 
-initial_conditions_trans = state_transformation([0,0,0], initial_conditions);
+initial_conditions_trans = state_transformation([0,0,0], initial_conditions_1);
 
 initial_conditions_trans_2 = state_transformation([0,0,0], initial_conditions_2);
 
