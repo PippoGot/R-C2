13651b65681f63f74259366460493e00c354296f
Edoardo Di Pietrantonio <citeracitera@gmail.com>
2023-12-24 17:58:55 +0100

    TEST BENCH ONLY on NON LINEAR TOTAL SYSTEM

--- init.m
+++ init.m
@@ -1,9 +1,7 @@
-initial_conditions_1 = [8, 8, pi*1.8];
-initial_conditions_2 = [8, 8, pi/2];
+initial_conditions_1 = [8, 8, 0];
+initial_conditions_2 = [6, 3, 2*pi];
 initial_conditions_3 = [-8, -8, 3*pi/2];
 
 initial_conditions_trans = state_transformation([0,0,0], initial_conditions_1);
-
 initial_conditions_trans_2 = state_transformation([0,0,0], initial_conditions_2);
-
 initial_conditions_trans_3 = state_transformation([0,0,0], initial_conditions_3);
