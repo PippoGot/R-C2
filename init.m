7aafbc813f98254593743537ff9bea22b5f9a60a
Filippo Gottardo <11got200@gmail.com>
2023-12-30 00:20:35 +0100

    modifiche per quality of life
    - cambio nome a "init.mlx" in "test_model.mlx";
    
    - creato file "full_system.slx" in cui vengono condensati:
     * "trajectory_tracking_linear_sub.slx";
     * "feed_lin_double_deriv_sub.slx";
     * "feed_lin_sagittal_sub.slx";
    
    - rimossi file non più necessari:
    per inutilizzo/obsolescenza
     * rendezvous_model.slx.r2020b;
     utilizzati in "posture_subsys.slx"
     * "posture_regulation.slx";
     * "rendezvous_model.slx";
    migliorati/reimplementati
     * "consensus.slx";
     * "controller_change.slx";
    utilizzati in strategie di trajectory tracking varie
     * "trajectory_tracking.slx";
    utilizzati in "full_system.slx" via matlab script
     * "linear_control_complete.slx";
     * "feed_lin_sagittal_control_complete.slx";
     * "feed_lin_deriv_control_complete.slx";
    
    - cambiamenti testati, resta da controllaretrajectory tracking con non-linear control;

--- init.m
+++ init.m
@@ -1,7 +1,35 @@
+%% Clearing
+
+clear 
+close all
+clc
+
+
+%% Control Strategy Selection
+
+posture = 'posture_subsys';
+trajectory = 'feed_lin_sagittal_sub';
+
+% vvv DON'T CHANGE THIS CODE IF YOU DON'T KNOW WHAT YOU ARE DOING !!! vvv
+
+% change the posture regulation strategy
+set_param('full_system/Agent 1/PR', 'ReferencedSubsystem', posture);
+set_param('full_system/Agent 2/PR', 'ReferencedSubsystem', posture);
+set_param('full_system/Agent 3/PR', 'ReferencedSubsystem', posture);
+
+% Change the trajectory tracking strategy
+set_param('full_system/Agent 1/TT', 'ReferencedSubsystem', trajectory);
+set_param('full_system/Agent 2/TT', 'ReferencedSubsystem', trajectory);
+set_param('full_system/Agent 3/TT', 'ReferencedSubsystem', trajectory);
+
+% ^^^ --------------------------------------------------------------- ^^^
+
+%% Agents Initial Conditions
+
 initial_conditions_1 = [8, 8, 0];
 initial_conditions_2 = [6, 3, 2*pi];
 initial_conditions_3 = [-8, -8, 3*pi/2];
 
-initial_conditions_trans = state_transformation([0,0,0], initial_conditions_1);
+initial_conditions_trans_1 = state_transformation([0,0,0], initial_conditions_1);
 initial_conditions_trans_2 = state_transformation([0,0,0], initial_conditions_2);
 initial_conditions_trans_3 = state_transformation([0,0,0], initial_conditions_3);
