1994210e5902f3a1d666f0c821586bb5903b7988
Filippo Gottardo <11got200@gmail.com>
2024-01-02 00:32:15 +0100

    riordinati file subsystem feed_lin
    altre modifiche al file principale per ottimizzare e migliorare la leggibilità degli schemi.
    
    implementato anche "non_linear_control_complete.slx" nel file di test "full_system.slx".

--- init.m
+++ init.m
@@ -4,26 +4,6 @@ clear
 close all
 clc
 
-
-%% Control Strategy Selection
-
-posture = 'posture_subsys';
-trajectory = 'feed_lin_sagittal_sub';
-
-% vvv DON'T CHANGE THIS CODE IF YOU DON'T KNOW WHAT YOU ARE DOING !!! vvv
-
-% change the posture regulation strategy
-set_param('full_system/Agent 1/PR', 'ReferencedSubsystem', posture);
-set_param('full_system/Agent 2/PR', 'ReferencedSubsystem', posture);
-set_param('full_system/Agent 3/PR', 'ReferencedSubsystem', posture);
-
-% Change the trajectory tracking strategy
-set_param('full_system/Agent 1/TT', 'ReferencedSubsystem', trajectory);
-set_param('full_system/Agent 2/TT', 'ReferencedSubsystem', trajectory);
-set_param('full_system/Agent 3/TT', 'ReferencedSubsystem', trajectory);
-
-% ^^^ --------------------------------------------------------------- ^^^
-
 %% Agents Initial Conditions
 
 initial_conditions_1 = [8, 8, 0];
@@ -33,3 +13,27 @@ initial_conditions_3 = [-8, -8, 3*pi/2];
 initial_conditions_trans_1 = state_transformation([0,0,0], initial_conditions_1);
 initial_conditions_trans_2 = state_transformation([0,0,0], initial_conditions_2);
 initial_conditions_trans_3 = state_transformation([0,0,0], initial_conditions_3);
+
+%% Control Strategy Selection
+
+posture = 'posture_subsys';
+trajectory = 'feed_lin_double_deriv_sub';
+% trajectory = 'feed_lin_sagittal_sub';
+distance_threshold = 4;
+
+% vvv DON'T CHANGE THIS CODE IF YOU DON'T KNOW WHAT YOU ARE DOING !!! vvv
+
+set_param('full_system', "FastRestart", "off");
+
+% Change the posture regulation strategy
+set_param('full_system/Agent 1/PR', 'ReferencedSubsystem', posture);
+
+% Change the trajectory tracking strategy
+set_param('full_system/Agent 1/TT', 'ReferencedSubsystem', trajectory);
+
+% Save model
+ save_system('full_system', 'SaveDirtyReferencedModels', 'on')
+
+% ^^^ --------------------------------------------------------------- ^^^
+
+
