ca89af92ec5a4cd47ea9ef5d580c034b92e27c2d
Filippo Gottardo <11got200@gmail.com>
2024-01-02 21:06:06 +0100

    completata la risistemazione dei file simulink
    eseguiti test di valutazione su tutte e 4 le strategie di controllo, con esito principalmente positivo.

--- init.m
+++ init.m
@@ -1,25 +1,51 @@
 %% Clearing
 
+
 clear 
 close all
 clc
 
+
 %% Agents Initial Conditions
+
 
 initial_conditions_1 = [8, 8, 0];
 initial_conditions_2 = [6, 3, 2*pi];
 initial_conditions_3 = [-8, -8, 3*pi/2];
 
+
 initial_conditions_trans_1 = state_transformation([0,0,0], initial_conditions_1);
 initial_conditions_trans_2 = state_transformation([0,0,0], initial_conditions_2);
 initial_conditions_trans_3 = state_transformation([0,0,0], initial_conditions_3);
 
+
 %% Control Strategy Selection
 
+
+% Posture Regulation Strategy
 posture = 'posture_subsys';
-trajectory = 'feed_lin_double_deriv_sub';
+
+
+% Trajectory Tracking Strategy
 % trajectory = 'feed_lin_sagittal_sub';
-distance_threshold = 4;
+% trajectory = 'feed_lin_double_deriv_sub';
+% trajectory = 'trajectory_tracking_linear_sub';
+trajectory = 'trajectory_tracking_non_linear_sub';
+
+
+% Parameters
+distance_threshold = 2;
+
+sagittal_K = [10; 5];
+
+double_deriv_Kp = [2 0; 0 5];
+double_deriv_Kd = [5 0; 0 5];
+
+csi = 1/sqrt(2);
+a = 5;
+
+b = 2;
+
 
 % vvv DON'T CHANGE THIS CODE IF YOU DON'T KNOW WHAT YOU ARE DOING !!! vvv
 
